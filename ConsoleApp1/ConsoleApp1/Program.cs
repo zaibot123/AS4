d87eaf61ccd69e5da2f0526a03dca06ec26296ea
Tobias <tobias@taekker.net>
2022-10-28 14:08:00 +0200

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+static void Main(string[] args)
+{
+    Rumst a=new Rumst([1,2,3])
+}
+public class Rumst
+{
+    public int[] b;
+
+    public Rumst(int[] b)
+    {
+        this.b = b;
+    }
+
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Rumst r = new Rumst(1);
+            Console.Write(r.b);
+        }
+    }
+
+    public class Rumst
+    {
+
+        public int b;
+
+        public Rumst(int b)
+        {
+            this.b = b;
+        }
+    }
+}
